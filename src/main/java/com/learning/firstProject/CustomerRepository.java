0550eeea046d16200c91f4516404f57d1c0a58b9
25Huseyn <huseynajafov@gmail.com>
2023-08-14 16:31:28 +0400

    First Project For Learning Spring

--- src/main/java/com/learning/firstProject/CustomerRepository.java
+++ src/main/java/com/learning/firstProject/CustomerRepository.java
@@ -3,6 +3,6 @@ package com.learning.firstProject;
 import com.learning.firstProject.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 }
